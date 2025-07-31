878cd9959132e1980e9d8f81645ef1d1ffd7a5e2
Mahdi Hassanzadeh <legion@Mahdis-MacBook-Air.local>
2025-07-31 16:14:36 +0330

    configuring env setting

--- mystorefront/settings/production.py
+++ mystorefront/settings/production.py
@@ -5,4 +5,15 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mystorefront.chbk.app']
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ['DATABASE_ENGINE'],
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': os.environ['DATABASE_PORT'],
+    }
+}
